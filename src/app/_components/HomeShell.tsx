df026cf630a331a751e441dd9bad7614bb1ea6b4
zelkovaria <hyein396@gmail.com>
2026-08-20 23:46:56 +0900

    feat: 홈 화면 레이아웃 구현

---
+++ src/app/_components/HomeShell.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import type { ReactNode } from "react";
+import { BottomNav } from "@/shared/ui/BottomNav";
+
+type HomeShellProps = {
+  children: ReactNode;
+};
+
+export function HomeShell({ children }: HomeShellProps) {
+  return (
+    <>
+      <div className="flex min-h-0 flex-1 flex-col overflow-y-auto">{children}</div>
+      <div className="flex shrink-0 justify-center px-ds-20 py-ds-12">
+        <BottomNav value="home" onValueChange={() => {}} onCreate={() => {}} />
+      </div>
+    </>
+  );
+}
